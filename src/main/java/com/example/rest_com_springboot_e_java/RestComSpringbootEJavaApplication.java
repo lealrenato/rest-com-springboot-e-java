43e8e7b45f9d7193fd159e0a6c6cec0f1a62efa6
Renato Leal <renatolealhiss@gmail.com>
2026-01-12 16:20:18 -0300

    inicial projeto

---
+++ src/main/java/com/example/rest_com_springboot_e_java/RestComSpringbootEJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.rest_com_springboot_e_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestComSpringbootEJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestComSpringbootEJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rest_com_springboot_e_java/RestComSpringbootEJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_com_springboot_e_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestComSpringbootEJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
